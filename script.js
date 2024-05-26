89784ed20168eed760fee4a3da36bad5c6f42673
NickDriesen <nick.driesen@leerling.vtiz.be>
2024-05-26 19:06:17 +0200

    Mediafolder added

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-var button = document.querySelector('#toggler');
+var img_array = ['Boot.jpg', 'background.jpg', 'http://placehold.it/100x100/red'];
+i = 0;
 
-button.onclick = function(){
- var img = document.querySelector('img');
- img.src = "new img url"
+function myFunction() {
+    i++;
+    document.getElementById("myImg").src = img_array[i];
+    if (i == img_array.length - 1) {
+        i = -1;
+
+    }
+
 }
