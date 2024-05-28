7d1d8283d02252faa16666f19841401890a8fcba
NickDriesen <nick.driesen@leerling.vtiz.be>
2024-05-28 09:53:26 +0200

    added box shadow

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var img_array = ['Media/Technicaldrawing-pully.png', 'Media/Technicaldrawing-wheenbracket.png', 'http://placehold.it/100x100/red'];
-var title_array = ['Pully', 'Wheelbracket', 'mast']
+var img_array = ['Media/Technicaldrawing-pully.png', 'Media/Technicaldrawing-wheenbracket.png', 'Media/test.jpg'];
+var title_array = ['Pully', 'Wheelbracket', 'test']
 var para_array = [
     'Voor de pully gebruikten we één algemeen ontwerp, dit werdt gebruikt voor de aandrijving aan de hand van rubbere bandjes. Dit ontwerp werdt gemaakt in solidworks en uitgewerkt met een 3D printer.',
     'Deze bracket wordt gebruikt om het wiel, dat het zeil op en neer haalt, op zijn plek te houden. Dit ontwerp werdt gemaakt in solidworks en uitgewerjt met een 3D printer.',
@@ -13,6 +13,8 @@ function nextFunction() {
 
     var element = document.getElementById("myPara");
     element.innerHTML = para_array[i];
+
+    document.getElementById("testvar").innerHTML = i;
 
     document.getElementById("myImg").src = img_array[i];
     if (i == img_array.length - 1) {
@@ -32,8 +34,23 @@ function prevFunction() {
 
     document.getElementById("myImg").src = img_array[i];
     if (i < 0) {
-        i = 0;
+        i = 1;
 
     }
 
+}
+
+document.getElementById("testvar").innerHTML = i;
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
